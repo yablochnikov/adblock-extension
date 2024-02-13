5e7c15026a2d2c1993b385a84e6fb36719afaa73
Yablochnikov <yablochnikovnikita@gmail.com>
2024-02-13 12:13:12 +0200

    adblock via manifest v2

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,17 @@
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
-    console.log(details);
+    const url = details.url;
+    const filters = ["googleadservices", "googlesyndication", "g.doubleclick"];
+    for (const filter of filters) {
+      if (url.indexOf(filter) != -1) {
+        return {
+          cancel: true,
+        };
+      }
+    }
+    return {
+      cancel: false,
+    };
   },
   {
     urls: ["<all_urls>"],
